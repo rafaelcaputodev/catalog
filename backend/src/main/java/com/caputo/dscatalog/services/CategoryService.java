ed553cf86cf0956e7275b7318adb91ad784b3f04
Rafael Caputo <rafaelcaputodev@gmail.com>
2021-03-31 22:29:25 -0300

    searching for category by id

--- backend/src/main/java/com/caputo/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/caputo/dscatalog/services/CategoryService.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.caputo.dscatalog.Repositories.CategoryRepository;
 import com.caputo.dscatalog.dto.CategoryDTO;
@@ -16,10 +17,15 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRep;
 	
+	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		List<Category> list = categoryRep.findAll();
-		
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-		
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Category entity =  categoryRep.findById(id).get();
+		return new CategoryDTO(entity);
 	}
 }
