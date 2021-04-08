c588cdcb7c905445fffcfa1ab070b2058da042c3
Rafael Caputo <rafaelcaputodev@gmail.com>
2021-04-08 17:52:08 -0300

    Delete category

--- backend/src/main/java/com/caputo/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/caputo/dscatalog/services/CategoryService.java
@@ -3,8 +3,11 @@ package com.caputo.dscatalog.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.caputo.dscatalog.services.exceptions.DatabaseException;
 import com.caputo.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -50,6 +53,18 @@ public class CategoryService {
 		}
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+    }
+
+    public void delete(Long id) {
+		try {
+			categoryRep.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e){
+			throw new DatabaseException("Integrity violation");
 		}
     }
 }
