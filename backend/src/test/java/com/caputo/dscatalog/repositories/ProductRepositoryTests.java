13292b2d39f31da26d027484bd35f09bee3bfd85
Rafael Caputo <rafaelcaputodev@gmail.com>
2022-01-10 21:37:28 -0300

    Test DeleteById

--- backend/src/test/java/com/caputo/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/caputo/dscatalog/repositories/ProductRepositoryTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.util.Optional;
 
@@ -18,12 +19,21 @@ public class ProductRepositoryTests {
     @Test
     public void deleteShouldDeleteObjectWhenIdExists(){
 
-        long exintingId = 1L;
+        long existingId = 1L;
 
-        repository.deleteById(exintingId);
+        repository.deleteById(existingId);
 
-        Optional<Product> result = repository.findById(exintingId);
+        Optional<Product> result = repository.findById(existingId);
 
         Assertions.assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist (){
+        long nonExistingId = 654654650;
+
+        Assertions.assertThrows(EmptyResultDataAccessException.class, () ->{
+            repository.deleteById(nonExistingId);
+        });
     }
 }
